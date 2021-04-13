0f784b91fa386054e43bc92fd1b24d25ed6f4410
Phillip Lassen <phillip@peaksandpies.com>
2021-04-13 12:16:17 +0200

    now endless args can be used when calling the logger

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const logger = require('./index.js')
 
-logger.error('warp nacelles offline');
-logger.info('shields at 99%');
-logger.debug('debug');
-logger.warn('warn');
-logger.verbose('verbose');
-logger.silly('silly');
+// logger.error('warp nacelles offline');
+// logger.info('shields at 99%', { test: true, meta: 'test'});
+logger.info('shields at 99%', { test: true, meta: 'test'}, 1, 2, 3, 4, 'hi', 'hey', 'ho');
+logger.info('shields at 99%', 1, 2, 3, 4, 'hi', 'hey', 'ho', { test: true, meta: 'test'});
+// logger.debug('debug');
+// logger.warn('warn');
+// logger.verbose('verbose');
+// logger.silly('silly');
