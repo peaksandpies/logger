0e9cd5cbe621492aa0e7db7e1b0392478158da40
Phillip Lassen <phillip@peaksandpies.com>
2021-04-23 08:48:08 +0200

    changed logging when node_run is not defined or different from standard

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const { createLogger } = require('winston')
 const { NODE_RUN } = process.env
 
-console.log(`NODE_RUN ${NODE_RUN}`)
 let logConfig
 try {
   logConfig = require(`./lib/runtime/${NODE_RUN}`)
 } catch (err) {
-  console.log(err)
+  console.log(`runtime ${NODE_RUN} not defined, using local`)
   logConfig = require(`./lib/runtime/local`)
 }
-
-const logger = createLogger(logConfig);
+const logger = createLogger(logConfig)
 
 module.exports = logger
--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 const logger = require('./index.js')
 
-// logger.error('warp nacelles offline');
-// logger.info('shields at 99%', { test: true, meta: 'test'});
-logger.info('shields at 99%', { test: true, meta: 'test'}, 1, 2, 3, 4, 'hi', 'hey', 'ho');
-logger.info('shields at 99%', 1, 2, 3, 4, 'hi', 'hey', 'ho', { test: true, meta: 'test'});
-// logger.debug('debug');
-// logger.warn('warn');
-// logger.verbose('verbose');
-// logger.silly('silly');
+let cnt = 10
+for (let i = 0; i<cnt ;i++) {
+  logger.info(`run ${i}`, { test: true, meta: 'test'}, 7);
+}
+
+logger.error('warp nacelles offline');
+logger.info('shields at 99%', { test: true, meta: 'test'}, 7);
+// logger.info(2, 'www')
+logger.debug('debug');
+logger.warn('warn');
+logger.verbose('verbose');
+logger.silly('silly');
