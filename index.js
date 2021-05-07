cf68c7d055980b26f481487a334a66fafd55a39c
mfeyx <github@feyx.de>
2021-05-07 14:57:10 +0200

    try catch block

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 'use strict'
-
 const { createLogger } = require('winston')
 const runtime = process.env.NODE_RUN || 'local'
-const logModule = `./lib/runtimes/${runtime}`
-const logConfig = require(logModule)
-
-// try {
-//   logConfig = require(`./lib/runtimes/${runtime}`)
-// } catch (err) {
-//   console.log(`runtime ${runtime} not defined, using local`)
-//   logConfig = require(`./lib/runtimes/local`)
-// }
-
+let logConfig
+try {
+  logConfig = require(`./lib/runtimes/${runtime}`)
+} catch (err) {
+  console.log(`runtime ${runtime} not defined, using local`)
+  logConfig = require('./lib/runtimes/local')
+}
 const logger = createLogger(logConfig)
-
 module.exports = logger
