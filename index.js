c5f0243bb5574366ffd5d662b2ca551a62f3646b
Ruxin <ruxin@peaksandpies.com>
2025-08-14 17:23:31 +0200

    formatting and add setGlobalLabels

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-'use strict'
-const { createLogger } = require('winston')
-const runtime = process.env.NODE_RUN || 'local'
-let logConfig
+'use strict';
+const { createLogger } = require('winston');
+const runtime = process.env.NODE_RUN || 'local';
+let logConfig;
 try {
-  logConfig = require(`./lib/runtimes/${runtime}`)
+  logConfig = require(`./lib/runtimes/${runtime}`);
 } catch (err) {
-  console.log(`runtime ${runtime} not defined, using local`)
-  logConfig = require('./lib/runtimes/local')
+  console.log(`runtime ${runtime} not defined, using local`);
+  logConfig = require('./lib/runtimes/local');
 }
-const logger = createLogger(logConfig)
-module.exports = logger
+
+const logger = createLogger(logConfig);
+
+const setGlobalLabels = (labels) => {
+  logger.defaultMeta = labels;
+};
+
+module.exports = {
+  logger,
+  setGlobalLabels,
+};
--- test.js
+++ test.js
@@ -1,14 +1,20 @@
-const logger = require('./index.js')
+const { setGlobalLabels, logger } = require('./index.js');
 
-let cnt = 10
-for (let i = 0; i<cnt ;i++) {
-  logger.info(`run ${i}`, { test: true, meta: 'test'}, 7)
+const myLabels = {
+  jobId: 'abc-123',
+};
+setGlobalLabels({ jobMeta: myLabels });
+let cnt = 10;
+for (let i = 0; i < cnt; i++) {
+  logger.info(`run ${i}`, { test: true, meta: 'test' }, 7);
 }
 
-logger.error('warp nacelles offline')
-logger.info('shields at 99%', { test: true, meta: 'test'}, 7)
+logger.error('warp nacelles offline');
+logger.info('shields at 99%', { test: true, meta: 'test' }, 7);
 // logger.info(2, 'www')
-logger.debug('debug')
-logger.warn('warn')
-logger.verbose('verbose')
-logger.silly('silly')
+logger.debug('debug');
+logger.warn('warn');
+logger.verbose('verbose');
+logger.silly('silly');
+
+logger.info('job');
